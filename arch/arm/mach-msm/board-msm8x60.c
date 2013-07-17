0c05a487f73ba9c347b320f371c9c0702d057ba8
jied83 <jied83@gmail.com>
2013-07-17 11:06:15 +0400

    fix compile
    Change-Id: Ib470f22b653d7320720773675daf881c1c4afc58

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -11204,7 +11204,7 @@ static struct msm_bus_scale_pdata dtv_bus_scale_pdata = {
 
 static struct lcdc_platform_data dtv_pdata = {
 	.bus_scale_table = &dtv_bus_scale_pdata,
-	.lcdc_power_save = hdmi_panel_power,
+//	.lcdc_power_save = hdmi_panel_power,
 };
 
 static struct msm_bus_paths dtv_hdmi_prim_bus_scale_usecases[] = {
